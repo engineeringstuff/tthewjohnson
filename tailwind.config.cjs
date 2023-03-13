864c7c875f6fde462456f6885a5b7e2c549a807b
engineeringstuff <292243+engineeringstuff@users.noreply.github.com>
2023-03-13 05:03:34 -0700

    Hyperlegible Font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				hyperlegible: ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };
